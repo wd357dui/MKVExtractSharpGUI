3b8f40bc18116c5d4b13089c54fa5b45914f4341
wd357dui <wd357dui@gmail.com>
2018-01-22 08:39:20 +0100

    Update GUI.cs
    add .mp3 extension

--- MKVExtractSharpGUI/GUI.cs
+++ MKVExtractSharpGUI/GUI.cs
@@ -178,6 +178,8 @@ namespace MKVExtractSharpGUI
                         extension = ".flac";
                     else if (array[i].Contains("Opus"))
                         extension = ".opus";
+                    else if (array[i].Contains("MPEG Audio")
+                        extension = ".mp3"
                     else if (array[i].Contains("VP8"))
                         extension = ".vp8";
                     else if (array[i].Contains("VP9"))
