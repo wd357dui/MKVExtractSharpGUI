f7138789f85ec507520e3c82a92c346fdf9d6714
wd357dui <wd357dui@gmail.com>
2018-01-22 07:30:29 +0100

    first update

---
+++ MKVExtractSharpGUI/GUI.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace MKVExtractSharpGUI
+{
+    partial class MkvExtractSharpGUI
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ExtractButton = new System.Windows.Forms.Button();
+            this.FileAddressInput = new System.Windows.Forms.TextBox();
+            this.InputFileLabel = new System.Windows.Forms.Label();
+            this.SelectFileButton = new System.Windows.Forms.Button();
+            this.AboutLabel = new System.Windows.Forms.Label();
+            this.ShowOutputsCheckBox = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // ExtractButton
+            // 
+            this.ExtractButton.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ExtractButton.Location = new System.Drawing.Point(12, 106);
+            this.ExtractButton.Name = "ExtractButton";
+            this.ExtractButton.Size = new System.Drawing.Size(280, 23);
+            this.ExtractButton.TabIndex = 5;
+            this.ExtractButton.Text = "Extract";
+            this.ExtractButton.UseVisualStyleBackColor = true;
+            this.ExtractButton.Click += new System.EventHandler(this.ExtractButton_Click);
+            // 
+            // FileAddressInput
+            // 
+            this.FileAddressInput.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.FileAddressInput.Location = new System.Drawing.Point(12, 24);
+            this.FileAddressInput.Name = "FileAddressInput";
+            this.FileAddressInput.Size = new System.Drawing.Size(225, 21);
+            this.FileAddressInput.TabIndex = 1;
+            // 
+            // InputFileLabel
+            // 
+            this.InputFileLabel.AutoSize = true;
+            this.InputFileLabel.Location = new System.Drawing.Point(12, 9);
+            this.InputFileLabel.Name = "InputFileLabel";
+            this.InputFileLabel.Size = new System.Drawing.Size(71, 12);
+            this.InputFileLabel.TabIndex = 0;
+            this.InputFileLabel.Text = "Input File:";
+            // 
+            // SelectFileButton
+            // 
+            this.SelectFileButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.SelectFileButton.Location = new System.Drawing.Point(243, 24);
+            this.SelectFileButton.Name = "SelectFileButton";
+            this.SelectFileButton.Size = new System.Drawing.Size(49, 21);
+            this.SelectFileButton.TabIndex = 2;
+            this.SelectFileButton.Text = "Select";
+            this.SelectFileButton.UseVisualStyleBackColor = true;
+            this.SelectFileButton.Click += new System.EventHandler(this.SelectFileButton_Click);
+            // 
+            // AboutLabel
+            // 
+            this.AboutLabel.AutoSize = true;
+            this.AboutLabel.Location = new System.Drawing.Point(12, 52);
+            this.AboutLabel.Name = "AboutLabel";
+            this.AboutLabel.Size = new System.Drawing.Size(251, 12);
+            this.AboutLabel.TabIndex = 3;
+            this.AboutLabel.Text = "MkvExtract GUI(written in C#) by wd357dui";
+            // 
+            // ShowOutputsCheckBox
+            // 
+            this.ShowOutputsCheckBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.ShowOutputsCheckBox.AutoSize = true;
+            this.ShowOutputsCheckBox.Location = new System.Drawing.Point(12, 84);
+            this.ShowOutputsCheckBox.Name = "ShowOutputsCheckBox";
+            this.ShowOutputsCheckBox.Size = new System.Drawing.Size(240, 16);
+            this.ShowOutputsCheckBox.TabIndex = 4;
+            this.ShowOutputsCheckBox.Text = "Show command line inputs and outputs";
+            this.ShowOutputsCheckBox.UseVisualStyleBackColor = true;
+            this.ShowOutputsCheckBox.CheckedChanged += new System.EventHandler(this.ShowOutputsCheckBox_CheckedChanged);
+            // 
+            // MkvExtractSharpGUI
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(304, 141);
+            this.Controls.Add(this.ShowOutputsCheckBox);
+            this.Controls.Add(this.AboutLabel);
+            this.Controls.Add(this.SelectFileButton);
+            this.Controls.Add(this.InputFileLabel);
+            this.Controls.Add(this.FileAddressInput);
+            this.Controls.Add(this.ExtractButton);
+            this.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MinimumSize = new System.Drawing.Size(320, 180);
+            this.Name = "MkvExtractSharpGUI";
+            this.Text = "MkvExtract GUI";
+            this.Load += new System.EventHandler(this.GUI_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ExtractButton;
+        private System.Windows.Forms.TextBox FileAddressInput;
+        private System.Windows.Forms.Label InputFileLabel;
+        private System.Windows.Forms.Button SelectFileButton;
+        private System.Windows.Forms.Label AboutLabel;
+        private System.Windows.Forms.CheckBox ShowOutputsCheckBox;
+    }
+}
+
---
+++ MKVExtractSharpGUI/GUI.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MKVExtractSharpGUI
+{
+    public partial class MkvExtractSharpGUI : Form
+    {
+        private Process mkvextract;
+        private Process mkvmerge;
+        static String _ = " ";
+
+        /// <summary>
+        /// if showIO is set to true,
+        /// show a MessageBox every time before/after
+        /// executing a mkvtoolnix program.
+        /// </summary>
+        private bool showIO;
+
+        public MkvExtractSharpGUI()
+        {
+            InitializeComponent();
+        }
+
+        private void GUI_Load(object sender, EventArgs e)
+        {
+            if (!(File.Exists("mkvextract.exe") && File.Exists("mkvmerge.exe")))
+            {
+                MessageBox.Show("Please put this application in the MKVToolNix working directory","Error");
+                this.Close();
+            }
+
+            showIO = false;
+            ShowOutputsCheckBox.Checked = false;
+
+            mkvextract = new Process();
+            mkvextract.StartInfo.FileName = "mkvextract.exe";
+            mkvextract.StartInfo.RedirectStandardInput = true;
+            mkvextract.StartInfo.RedirectStandardOutput = true;
+            mkvextract.StartInfo.CreateNoWindow = true;
+            mkvextract.StartInfo.UseShellExecute = false;
+
+            mkvmerge = new Process();
+            mkvmerge.StartInfo.FileName = "mkvmerge.exe";
+            mkvmerge.StartInfo.RedirectStandardInput = true;
+            mkvmerge.StartInfo.RedirectStandardOutput = true;
+            mkvmerge.StartInfo.CreateNoWindow = true;
+            mkvmerge.StartInfo.UseShellExecute = false;
+        }
+
+        /// <summary>
+        /// Allow users to select a file with a OpenFileDialog.
+        /// </summary>
+        private void SelectFileButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog();
+            open.Filter = "Matroska file|*.mkv;*.mka;*.mk3d;*.mks";
+            open.Multiselect = false;
+            if (open.ShowDialog() == DialogResult.OK)
+            {
+                FileAddressInput.Text = open.FileName;
+            }
+        }
+
+        /// <summary>
+        /// Allow users to drag & drop a file into the application.
+        /// </summary>
+        private void MkvExtractSharpGUI_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.Copy;
+            }
+            else
+            {
+                e.Effect = DragDropEffects.None;
+            }
+        }
+
+        /// <summary>
+        /// Handling the dropped file,
+        /// write the path of the first file into the input textbox.
+        /// </summary>
+        private void MkvExtractSharpGUI_DragDrop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+                this.FileAddressInput.Text = files[0];
+            }
+        }
+
+        private void ExtractButton_Click(object sender, EventArgs e)
+        {
+            ExtractButton.Enabled = false;
+            ExtractOperation();
+        }
+
+        private void ShowOutputsCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            /// <summary>
+            /// if showIO is set to true,
+            /// show a MessageBox every time before/after
+            /// executing a mkvtoolnix program.
+            /// </summary>
+            showIO = ShowOutputsCheckBox.Checked;
+        }
+
+        private void ExtractOperation()
+        {
+            String MkvFile = FileAddressInput.Text;
+            MkvFile.Replace(@"\", "/");
+
+            if (!File.Exists(MkvFile))
+            {
+                MessageBox.Show("File does not exist", "Error");
+                ExtractButton.Enabled = true;
+                return;
+            }
+            
+            String FileNoExtension = MkvFile.Remove(MkvFile.LastIndexOf("."));
+
+            StartExtract(_ + "\"" + MkvFile + "\"" + _ + "tracks" + _ + TrackList(MkvFile, FileNoExtension));
+            ExtractButton.Enabled = true;
+        }
+
+        /// <summary>
+        /// Using mkvmerge to determine all the tracks of the specified file,
+        /// and set output paths for each track.
+        /// </summary>
+        /// <param name="File">the file path</param>
+        /// <param name="FileNoExtension">the file path without extension name</param>
+        /// <returns></returns>
+        private string TrackList(string File, string FileNoExtension)
+        {
+            string arguments = _ + "\"" + File + "\"" + " -i --ui-language english";
+            if (showIO)
+                MessageBox.Show(arguments, "Executing mkvmerge with following arguments");
+            mkvmerge.StartInfo.Arguments = arguments;
+            mkvmerge.Start();
+            // Read the output in UTF-8 encoding.
+            // proved having problems decoding Chinese
+            // (or other languages') characters while not using UTF-8.
+            StreamReader reader = new StreamReader(mkvmerge.StandardOutput.BaseStream, Encoding.UTF8);
+            string output = reader.ReadToEnd();
+            reader.Close();
+            mkvmerge.StandardInput.Close();
+            mkvmerge.WaitForExit();
+            if (showIO)
+                MessageBox.Show(output, "mkvmerge output");
+            string[] array = output.Split('\n');
+
+            List<string> list = new List<string>();
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i].StartsWith("Track ID "))
+                {
+                    string num = array[i].Remove(0, 9);
+                    int number = int.Parse(num.Remove(num.IndexOf(":")));
+                    string extension = "";
+                    if (array[i].Contains("h.264"))
+                        extension = ".h264";
+                    else if (array[i].Contains("h.265"))
+                        extension = ".h265";
+                    else if (array[i].Contains("AAC"))
+                        extension = ".aac";
+                    else if (array[i].Contains("AC3"))
+                        extension = ".ac3";
+                    else if (array[i].Contains("FLAC"))
+                        extension = ".flac";
+                    else if (array[i].Contains("Opus"))
+                        extension = ".opus";
+                    else if (array[i].Contains("VP8"))
+                        extension = ".vp8";
+                    else if (array[i].Contains("VP9"))
+                        extension = ".vp9";
+                    else if (array[i].Contains("UTF-8"))
+                        extension = ".srt";
+                    list.Add(number + ":" + "\"" + FileNoExtension + "_track" + number + extension + "\"");
+                }
+            }
+            string tracks = "";
+            for (int i = 0; i < list.Count; i++)
+            {
+                tracks += _ + list[i] + _;
+            }
+            return tracks;
+        }
+
+        /// <summary>
+        /// Using mkvextract to extract the tracks
+        /// </summary>
+        /// <param name="arguments">arguments that will be given to mkvextract</param>
+        private void StartExtract(String arguments)
+        {
+            if (showIO)
+                MessageBox.Show(arguments, "Executing mkvextract with following arguments");
+            mkvextract.StartInfo.Arguments = arguments;
+            mkvextract.Start();
+            if (showIO)
+            {
+                StreamReader reader = new StreamReader(mkvextract.StandardOutput.BaseStream, Encoding.UTF8);
+                string output = reader.ReadToEnd();
+                reader.Close();
+                MessageBox.Show(output, "mkvextract output");
+            }
+            mkvextract.StandardInput.Close();
+            mkvextract.WaitForExit();
+        }
+    }
+}
